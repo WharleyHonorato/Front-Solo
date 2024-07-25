d1cb5ab04ec3acfe7f2e5df7a454759c200870d0
WharleyHonorato <wharleygrant@gmail.com>
2024-07-25 13:02:19 -0300

    fim

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import NavBar from "./components/NavBar"
 import Home from "./pages/Home"
 import Produto from "./pages/Produto"
 import Produtos from "./pages/Produtos"
-import Login from "./pages/Login"
 
 
 function App() {
 
   return (
     <div className="">
-      <Login/>
+      <NavBar/>
+      <Home/>
+      <Produto/>
+      <Produtos/>
       <Footer/>
     </div>
   )
